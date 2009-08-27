137b3c489fced6443aa428ab91a77435108da689
Lourens Naudé <lourens@methodmissing.com>
2009-08-28 00:03:03 +0100

    Initial prototype, win!

--- ext/channel/channel.c
+++ ext/channel/channel.c
@@ -2,6 +2,7 @@
 
 typedef struct {
 	int size;
+	int sbs;
     VALUE *subscribers;
 } RChannel;
 
@@ -9,6 +10,7 @@ typedef struct {
 #define GetChannelStruct(obj)	(Check_Type(obj, T_DATA), (RChannel*)DATA_PTR(obj))
 
 VALUE rb_cChannel;
+static ID id_call;
 
 static void mark_channel(RChannel* ch)
 {
@@ -24,12 +26,13 @@ static VALUE channel_alloc _((VALUE));
 static VALUE
 channel_alloc( VALUE klass )
 {
-	VALUE ch;
-	RChannel* chs;
-	ch = Data_Make_Struct(klass, RChannel, mark_channel, free_channel, chs);
-	chs->size = 0;
-	chs->subscribers = 0;
-	
+    VALUE ch;
+    RChannel* chs;
+    ch = Data_Make_Struct(klass, RChannel, mark_channel, free_channel, chs);
+    chs->size = 0;
+    chs->sbs = 0;
+    chs->subscribers = 0;
+   
     return ch;
 }
 
@@ -38,38 +41,72 @@ rb_channel_new(VALUE ch, int size)
 {
     RChannel* chs = GetChannelStruct(ch);
     if (size < 0 || size > MAX_CHANNEL_SIZE){ 
-	  rb_raise(rb_eArgError, "Invalid channel size!");
+      rb_raise(rb_eArgError, "Invalid channel size!");
     }
     if (size == 0) size++;
-	chs->size = size;
+    chs->size = size;
     chs->subscribers = ALLOC_N(VALUE, size);
-	return ch;  
+    return ch;  
 }
 
 static VALUE 
 rb_channel_initialize( int argc, VALUE *argv, VALUE ch )
 {
-	VALUE size;
-	int channel_size;
-    RChannel* chs = GetChannelStruct(ch);    
+    VALUE size;
+    int channel_size;  
     rb_scan_args(argc, argv, "01", &size);
-	channel_size = NIL_P(size) ? 0 : FIX2INT(size);
-	return rb_channel_new(ch, channel_size); 
+    channel_size = NIL_P(size) ? 0 : FIX2INT(size);
+    return rb_channel_new(ch, channel_size); 
 }
 
 static VALUE 
 rb_channel_size( VALUE ch )
 {
     RChannel* chs = GetChannelStruct(ch);    
-	return INT2FIX(chs->size); 
+    return INT2FIX(chs->size); 
+}
+
+static VALUE 
+rb_channel_subscribers( VALUE ch )
+{
+    RChannel* chs = GetChannelStruct(ch);    
+    return INT2FIX(chs->sbs); 
+}
+
+static VALUE 
+rb_channel_subscribe( int argc, VALUE *argv, VALUE ch )
+{
+    VALUE cb;
+    RChannel* chs = GetChannelStruct(ch);    
+    if (!rb_block_given_p()) rb_raise(rb_eArgError, "Block callback required!");
+    cb = rb_block_proc();
+    chs->subscribers[chs->sbs] = cb;
+    chs->sbs++;
+    return ch;
+}
+
+static VALUE 
+rb_channel_push( VALUE ch, VALUE obj )
+{
+    int i;
+    RChannel* chs = GetChannelStruct(ch);    
+    for (i=0; i < chs->sbs; i++) {
+      rb_funcall(chs->subscribers[i],id_call,1,obj);
+    }
+    return ch;
 }
 
 void
 Init_channel()
 { 
+    id_call = rb_intern("call");
+
     rb_cChannel  = rb_define_class("Channel", rb_cObject);
     rb_define_alloc_func(rb_cChannel, channel_alloc);
     
     rb_define_method(rb_cChannel,"initialize", rb_channel_initialize, -1);
     rb_define_method(rb_cChannel,"size", rb_channel_size, 0);
+    rb_define_method(rb_cChannel,"subscribers", rb_channel_subscribers, 0);
+    rb_define_method(rb_cChannel,"subscribe", rb_channel_subscribe, -1);
+    rb_define_method(rb_cChannel,"<<", rb_channel_push, 1);
 }
--- test/test_channel.rb
+++ test/test_channel.rb
@@ -12,5 +12,22 @@ class TestChannel < Test::Unit::TestCase
   def test_size
     assert_equal 10, Channel.new(10).size
   end
-    
+  
+  def test_subscribers
+    assert_equal 0, Channel.new(10).subscribers
+  end
+  
+  def test_subscribe
+    assert_equal 1, Channel.new.subscribe{|obj| obj }.subscribers
+    assert_raises(ArgumentError){ Channel.new.subscribe }
+    assert_raises(ArgumentError){ Channel.new.subscribe(:arg) }    
+  end 
+  
+  def test_push
+    @counter = 0
+    ch = Channel.new.subscribe{|obj| @counter += obj }
+    ch << 3
+    ch << -2
+    assert_equal 1, @counter
+  end
 end
